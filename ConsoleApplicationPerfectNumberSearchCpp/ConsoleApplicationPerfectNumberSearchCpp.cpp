afba19fa0b3276bfa335f1ecee62ac796608c170
Freddy Juhel <ext.freddy.juhel@reseau.sncf.fr>
2026-08-05 17:17:42 +0200

    clean code

--- ConsoleApplicationPerfectNumberSearchCpp/ConsoleApplicationPerfectNumberSearchCpp.cpp
+++ ConsoleApplicationPerfectNumberSearchCpp/ConsoleApplicationPerfectNumberSearchCpp.cpp
@@ -96,11 +96,11 @@ int main()
 		}
 		else
 		{
-			/*cout << i << " n'est pas un nombre parfait.";
-			cout << " Ses diviseurs sont: ";
+			cout << i << " n'est pas un nombre parfait.";
+			/*cout << " Ses diviseurs sont: ";
 			printDivisors(removeLastValue(divisors));
-			cout << " et la somme de ses diviseurs est: " << calculatedSum;
-			cout << endl;*/
+			cout << " et la somme de ses diviseurs est: " << calculatedSum;*/
+			cout << endl;
 		}
 	}
 
@@ -121,7 +121,7 @@ int main()
 // 33 550 336
 // 8 589 869 056
 // 137 438 691 328
-//  2 305 843 008 139 952 128
+// 2 305 843 008 139 952 128
 // 18 446 744 073 709 551 615 unsigned long long max value is not a perfect number
 // 2 658 455 991 569 831 744 654 692 615 953 842 176
 // 191 561 942 608 236 107 294 793 378 084 303 638 130 997 321 548 169 216
