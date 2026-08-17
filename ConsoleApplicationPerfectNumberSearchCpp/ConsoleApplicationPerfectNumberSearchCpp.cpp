2af65f6a4d3ffa221798acc02861ef3a88451932
fred <domaine75@gmail.com>
2026-08-17 19:25:38 +0200

    limit to int max value

--- ConsoleApplicationPerfectNumberSearchCpp/ConsoleApplicationPerfectNumberSearchCpp.cpp
+++ ConsoleApplicationPerfectNumberSearchCpp/ConsoleApplicationPerfectNumberSearchCpp.cpp
@@ -79,7 +79,7 @@ int main()
 	vector<string> result;
 	ostringstream sb;
   int startNumber = 33550337; // 33 550 336; 
-	int finalNumber = 8589869057; // 8 589 869 056;
+	int finalNumber = numeric_limits<int>::max(); //8589869057; // 8 589 869 056;
 	sb << "Liste des nombres parfaits entre " << startNumber << " et " << finalNumber << " sont: ";
 	vector<string> perfectNumbers;
 	perfectNumbers.push_back("Les nombres parfaits sont des nombres qui sont égaux à la somme de leurs diviseurs propres.");
